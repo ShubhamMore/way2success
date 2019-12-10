ed8b308401983d2b1c947b83f97a55e692e826d4
ShubhamMore <shubhammore1796@gmail.com>
2019-12-10 11:21:30 +0530

    Class Management

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   encKey: 'b417cf7714f5c803ac31ea7ea4ee3a11',
-  backend: 'https://way2success-backend.herokuapp.com/'
+  backend: 'http://13.233.136.101/:3000/'
 };
