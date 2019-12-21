a63b546ded61a1a4bcd797f03c9631212e106595
ShubhamMore <shubhammore1796@gmail.com>
2019-12-21 15:54:10 +0530

    way2success

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   encKey: 'b417cf7714f5c803ac31ea7ea4ee3a11',
-  backend: 'http://35.154.130.138:3000/'
+  backend: 'https://api.way2success.education/'
 };
