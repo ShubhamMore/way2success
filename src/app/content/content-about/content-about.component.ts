8cb25643cf41645c43060ce49c660a93321c9d6b
Sarvesh Nerurkar <sarvesh10n@gmail.com>
2020-01-01 20:59:19 +0530

    topper part added

--- src/app/content/content-about/content-about.component.ts
+++ src/app/content/content-about/content-about.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AboutService } from '../../services/about.service';
-import { AboutModel } from '../../models/about.model';
+import { AboutModel } from '../../models/about.model'
+import { TopperService } from 'src/app/services/topper.service';;
+import { TopperModel } from 'src/app/models/topper.model';
 
 @Component({
   selector: 'app-content-about',
@@ -11,14 +13,27 @@ export class ContentAboutComponent implements OnInit {
   loading: boolean;
   error: string;
   about: AboutModel;
+  toppers:TopperModel;
 
-  constructor(private aboutService: AboutService) {}
+  constructor(private aboutService: AboutService,private topperService: TopperService) {}
 
   ngOnInit() {
     this.loading = true;
     this.aboutService.getAbout().subscribe(
       (resData: any) => {
         this.about = resData;
+        this.loading = false;
+      },
+      (error: any) => {
+        this.error = error;
+        this.loading = false;
+      }
+    );
+
+    this.topperService.getToppers()
+    .subscribe(
+      (resData: any) => {
+        this.toppers = resData;
         this.loading = false;
       },
       (error: any) => {
